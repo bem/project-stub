7e509cfc9edc20ec49c00b97077b9222dd7022f5
Vladimir Grinenko <i@tadatuta.com>
2013-07-22 15:47:53 +0300

    Temporary fix on bem-core rev before v2 techs rollback

--- .bem/make.js
+++ .bem/make.js
@@ -12,7 +12,7 @@ MAKE.decl('Arch', {
     bundlesLevelsRegexp: /^.+?\.bundles$/,
 
     libraries: [
-        'bem-core @ v1',
+        'bem-core @ 3090cd35889fb12131cb0a77b708f7c1e5b6e008',
         'bem-controls @ v2'
     ]
 
