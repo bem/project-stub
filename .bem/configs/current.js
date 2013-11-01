04982ebe50c6c8e8545a3f28b22294a15c847dea
Varya Stepanova <mail@varya.me>
2013-11-01 14:26:42 +0100

    Bower made repo.db and configs unnecessary

--- .bem/configs/current.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    libraries : require('bem').util.extend(require('../repo.db'), {
-        'bem-controls': {
-            type     : 'git',
-            url      : 'git://github.com/bem/bem-controls.git',
-            npmPackages: false
-        }
-    })
-}
