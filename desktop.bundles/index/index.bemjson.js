cb53b4c49b911c97f94f51cdb9cb3c6d30a3aee3
Vladimir Grinenko <i@tadatuta.com>
2014-03-17 12:55:17 +0200

    Update index.bemjson.js stub

--- desktop.bundles/index/index.bemjson.js
+++ desktop.bundles/index/index.bemjson.js
@@ -3,11 +3,14 @@
     title: 'Title of the page',
     favicon: '/favicon.ico',
     head: [
-        { elem: 'css', url: '_index.css', ie: false },
-        { elem: 'css', url: '_index.ie.css', ie: 'gte IE 6' },
         { elem: 'meta', attrs: { name: 'description', content: '' }}
     ],
-    content:[
+    styles: [
+        { elem: 'css', url: '_index.css', ie: false },
+        { elem: 'css', url: '_index.ie.css', ie: 'IE' }
+    ],
+    scripts: [{ elem: 'js', url: '_index.js' }],
+    content: [
         {
             block: 'header',
             content: [
@@ -25,7 +28,6 @@
             content: [
                 'footer content goes here'
             ]
-        },
-        { elem: 'js', url: '_index.js' }
+        }
     ]
 })
