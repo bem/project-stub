b6441cb2389418e8863fc5d5b3337b2a450bca44
Vladimir Grinenko <i@tadatuta.com>
2014-04-12 01:43:47 +0400

    Move level config to make.js

--- .bem/make.js
+++ .bem/make.js
@@ -1,19 +1,20 @@
-/* jshint node:true */
-/* global MAKE */
+/* global MAKE:false */
 
-//process.env.YENV = 'production';
+// process.env.YENV = 'production';
+
+var PATH = require('path');
 
 MAKE.decl('Arch', {
 
-    blocksLevelsRegexp: /^.+?\.blocks/,
-    bundlesLevelsRegexp: /^.+?\.bundles$/
+    blocksLevelsRegexp : /^.+?\.blocks/,
+    bundlesLevelsRegexp : /^.+?\.bundles$/
 
 });
 
 
 MAKE.decl('BundleNode', {
 
-    getTechs: function() {
+    getTechs : function() {
 
         return [
             'bemjson.js',
@@ -26,6 +27,29 @@ MAKE.decl('BundleNode', {
             'html'
         ];
 
+    },
+
+    getLevelsMap : function() {
+        return {
+            desktop : [
+                'libs/bem-core/common.blocks',
+                'libs/bem-core/desktop.blocks',
+                'libs/bem-components/common.blocks',
+                'libs/bem-components/desktop.blocks',
+                'common.blocks',
+                'desktop.blocks'
+            ]
+        };
+    },
+
+     getLevels : function() {
+        var resolve = PATH.resolve.bind(PATH, this.root),
+            buildLevel = this.getLevelPath().split('.')[0],
+            levels = this.getLevelsMap()[buildLevel] || [];
+
+        return levels
+            .map(function(path) { return resolve(path); })
+            .concat(resolve(PATH.dirname(this.getNodePrefix()), 'blocks'));
     }
 
 });
