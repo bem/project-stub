51bef998f98056d311ebbe68278d382c1944328f
Vladimir Grinenko <i@tadatuta.com>
2014-04-25 01:00:50 +0400

    index.bemjson.js: Add meta viewport

--- desktop.bundles/index/index.bemjson.js
+++ desktop.bundles/index/index.bemjson.js
@@ -3,7 +3,8 @@
     title: 'Title of the page',
     favicon: '/favicon.ico',
     head: [
-        { elem: 'meta', attrs: { name: 'description', content: '' }},
+        { elem: 'meta', attrs: { name: 'description', content: '' } },
+        { elem: 'meta', attrs: { name: 'viewport', content: 'width=device-width, initial-scale=1' } },
         { elem: 'css', url: '_index.css' }
     ],
     scripts: [{ elem: 'js', url: '_index.js' }],
