957ae56a84ce570a49ac0a3bca88072aeafd2871
Vladimir Grinenko <i@tadatuta.com>
2014-06-22 03:00:30 +0400

    index.bemjson.js: Add _theme_normal to page block

--- desktop.bundles/index/index.bemjson.js
+++ desktop.bundles/index/index.bemjson.js
@@ -8,6 +8,7 @@
         { elem: 'css', url: '_index.css' }
     ],
     scripts: [{ elem: 'js', url: '_index.js' }],
+    mods: { theme: 'normal' },
     content: [
         {
             block: 'header',
