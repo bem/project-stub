515a558960eede75d1e41698d6f6778b3efca956
L0stSoul <LostSoul@beyondtheclouds.net>
2014-07-15 14:24:55 +0400

    revert 90f986d7

--- desktop.bundles/index/index.bemjson.js
+++ desktop.bundles/index/index.bemjson.js
@@ -5,9 +5,9 @@
     head: [
         { elem: 'meta', attrs: { name: 'description', content: '' } },
         { elem: 'meta', attrs: { name: 'viewport', content: 'width=device-width, initial-scale=1' } },
-        { elem: 'css', url: 'index/_index.css' }
+        { elem: 'css', url: '_index.css' }
     ],
-    scripts: [{ elem: 'js', url: 'index/_index.js' }],
+    scripts: [{ elem: 'js', url: '_index.js' }],
     mods: { theme: 'normal' },
     content: [
         {
