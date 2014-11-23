a52f0086b1a5e73cc5f6fbd7f36503e2d2e24f5d
Vladimir Grinenko <i@tadatuta.com>
2014-11-23 07:22:22 +0300

    use bem-components with php templates, switch to data-bem attr

--- .bem/techs/bh.php.js
+++ .bem/techs/bh.php.js
@@ -16,7 +16,8 @@ exports.techMixin = {
         return [
             '<?php',
             process.env.INCLUDE_PHP_BEM_BH === 'false' ? '' : 'require_once __DIR__ . "/../../vendor/php-bem-bh/index.php";',
-            '$bh = new BEM\\BH();'
+            '$bh = new BEM\\BH();',
+            '$bh->setOptions(["jsAttrName" => "data-bem", "jsAttrScheme" => "json"]);'
         ].join('\n');
     },
 
