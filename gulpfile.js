c9d96ea82791ec8f7c12ef6fc840a2bba0ca7bb9
Sergey Belov <peimei@ya.ru>
2015-12-12 18:22:15 +0300

    wip

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp'),
     gutil = require('gulp-util'),
     uglify = require('gulp-uglify'),
     concat = require('gulp-concat'),
+    concatStream = require('concat-stream'),
     lazypipe = require('lazypipe'),
     bemjsonToDecl = require('bemjson-to-decl'),
     through = require('through2'),
@@ -9,6 +10,7 @@ var gulp = require('gulp'),
     walk = require('bem-walk'),
     bemDeps = require('@bem/deps'),
     toArray = require('stream-to-array'),
+    path = require('path'),
     PluginError = gutil.PluginError;
 
 var walkConfig = {
@@ -31,7 +33,6 @@ var levels = [
     'desktop.blocks'
 ];
 
-// give lazypipe
 var jsTransform = lazypipe()
     .pipe(uglify);
 
@@ -48,16 +49,10 @@ function bundles() {
             var newFile = file.clone({contents: false});
             newFile.data = bemjsonToDecl.convert(require(file.path));
             newFile.contents = null;
-            // newFile.extname = '.decl.js'; // TODO: replace full extension
+            newFile.path = file.path.replace('.bemjson.js', '.decl.js');
             cb(null, newFile);
         }))
         .pipe(through.obj(function(file, enc, cb) {
-            if (file.isNull()) {
-                cb(null, file);
-            }
-
-            var self = this;
-
             toArray(bemDeps.load({levels: levels}), function(err, relations) {
                 if (err) {
                     return cb(err);
@@ -69,30 +64,27 @@ function bundles() {
                         return cb(err);
                     }
 
-                    filterDeps(deps.entities, fsEntities, 'js')
-                        .then(function(readyToConcat) {
-                            var paths = readyToConcat.map(function(p) {
-                                return p.path;
-                            });
+                    filterDeps(deps.entities, fsEntities, 'js', function(err, readyToConcat) {
+                        var paths = readyToConcat.map(function(p) {
+                            return path.relative(file.base, p.path);
+                        });
 
-                            var newFilePath = file.path.replace('.bemjson.js', '.js');
-                            var stream = gulp.src(paths).pipe(concat(newFilePath));
-                            stream.on('data', function(data) {
-                                setImmediate(function() {
-                                    cb(null, data);
-                                });
-                            });
-                            stream.on('end', cb);
-                            stream.on('error', cb);
-                        })
-                        .fail(cb);
+                        console.log(paths);
+
+                        gulp.src(paths)
+                            .pipe(concat(file.relative.replace('.decl.js', '.js')))
+                            .pipe(through.obj(function(newFile, enc, newCb) {
+                                console.log(newFile.base);
+                                console.log(newFile.path);
+                                console.log(newFile.relative);
+                                cb(null, newFile);
+                                newCb();
+                            }));
                     });
+                });
             });
         }))
-        .pipe(through.obj(function(file, enc, cb) {
-            console.log(file);
-            cb(null, file);
-        }))
+        .pipe(jsTransform())
         .pipe(gulp.dest('build'));
 }
 
@@ -105,24 +97,23 @@ function clean() {
  * @param  {Array} deps        – bem-deps [{ block, elem, modName, modVal }, ...]
  * @param  {Array} fsEntities  – bem-walk [{ entity: { block, elem, modName, modVal }, tech }, ...]
  * @param  {String} tech       - tech name: 'js' || 'css' || 'bemhtml' || ...
- * @return {Array}             - filtred deps with files
+ * @param  {Function} cb       - callback with filtred deps with files
  */
-function filterDeps(deps, fsEntities, tech) {
-    return new Promise(function(resolve, reject) {
-        var entitiesWithTech = [];
-        deps.forEach(function(entity) {
-            var ewt = fsEntities.filter(function(o) {
-                if(o.tech !== tech) return;
-                if(o.entity.block !== entity.block) return;
-                if(o.entity.elem !== entity.elem) return;
-                if(o.entity.modName !== entity.modName) return;
-                if(o.entity.modVal !== entity.modVal) return;
-                return true;
-            });
+function filterDeps(deps, fsEntities, tech, cb) {
+    var entitiesWithTech = [];
 
-            entitiesWithTech = [].concat(entitiesWithTech, ewt);
+    deps.forEach(function(entity) {
+        var ewt = fsEntities.filter(function(o) {
+            if(o.tech !== tech) return;
+            if(o.entity.block !== entity.block) return;
+            if(o.entity.elem !== entity.elem) return;
+            if(o.entity.modName !== entity.modName) return;
+            if(o.entity.modVal !== entity.modVal) return;
+            return true;
         });
 
-        resolve(entitiesWithTech);
+        entitiesWithTech = [].concat(entitiesWithTech, ewt);
     });
+
+    cb(null, entitiesWithTech);
 }
