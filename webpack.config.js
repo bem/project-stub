4ac4125576b45a82dc1ea36831fff94b7ffd383c
Alexey Litvinov <sullenor@yandex-team.ru>
2015-12-12 15:48:10 +0300

    Сборка bh

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,7 @@ module.exports = {
         loaders: [
             {
                 test: /\.bemjson.js$/,
-                loader: 'loaders/bem-extractor?tech[]=styl&tech[]=js!loaders/bemjson'
+                loader: 'loaders/bem-extractor?tech[]=bh.js&tech[]=js&tech[]=styl!loaders/bemjson'
             },
             {
                 test: /\.styl$/,
