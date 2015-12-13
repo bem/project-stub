b3cfb6b7857b135a915f9f82b654ea28b082fb29
Alexey Litvinov <sullenor@yandex-team.ru>
2015-12-13 12:00:52 +0300

    Обновил зависимости и сервер

--- app.js
+++ app.js
@@ -1,63 +1,7 @@
-var fs = require('fs'),
-    path = require('path'),
-    express = require('express'),
-    bodyParser = require('body-parser'),
-    favicon = require('serve-favicon'),
-    serveStatic = require('serve-static'),
-    morgan = require('morgan'),
-    app = express(),
+const express = require('express');
+const path = require('path');
 
-    rootDir = path.join(__dirname, '..'),
-    portOrSocket = 8085;
+const app = express();
 
-var router = require('express').Router();
-
-router.get('/', function(req, res) {
-    res.send('HELLO MFUCKERS');
-});
-
-app
-    .disable('x-powered-by')
-    .enable('trust proxy')
-    .use(morgan('combined'))
-    .use(serveStatic(path.join(rootDir, 'dist')))
-    .use(bodyParser.json())
-    .use(bodyParser.urlencoded({ extended: true }))
-    .use(require('cookie-parser')())
-    .use(router)
-    .use(function(req, res) {
-        res.sendStatus(404);
-    })
-    .use(function(err, req, res, next) {
-        console.error(err.stack);
-        res.sendStatus(500);
-    });
-
-exports.start = function() {
-    app
-        .listen(portOrSocket, function() {
-            console.info('app started on %s', portOrSocket);
-            isNaN(portOrSocket) && fs.chmod(portOrSocket, '0777');
-        })
-        .once('error', function(err) {
-            console.error('worker %s has failed to start application', process.pid);
-
-            if (err.code === 'EADDRINUSE') {
-                console.error('port or socket %s is taken', portOrSocket);
-                process.kill();
-                return;
-            }
-
-            console.log(err.stack);
-        });
-};
-
-if (!module.parent) {
-    if (fs.existsSync(portOrSocket)) {
-        try {
-            fs.unlinkSync(portOrSocket);
-        } catch (e) {}
-    }
-
-    exports.start();
-}
+app.use(express.static(path.join(__dirname, 'dist')));
+app.listen(8085);
