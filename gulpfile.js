0eb80bcc63e4508406321be6a594237218c188f8
blond <andrewblond@yandex.com>
2015-12-13 21:47:36 +0300

    wip

--- gulpfile.js
+++ gulpfile.js
@@ -26,53 +26,47 @@ var bemConfig = {
 };
 
 // building one bundle with waiting for completing all substreams
-gulp.task('build-the-rest', function () {
+gulp.task('jscss', function () {
     var bemProject = new gbem(bemConfig);
 
     return bemProject.init()
         .then(function() {
-            var res = [];
             var bem = bemProject.bundle({
                 name: 'index',
                 path: 'desktop.bundles/index',
-                decl: 'desktop.bundles/index/index.bemjson.js',
-//                levels: Object.levels
+                decl: 'desktop.bundles/index/index.bemjson.js'
             });
 
-            res.push(gmerge(
-                gulp.src('node_modules/ym/modules.js'),
-                bem.src({tech: 'js', extensions: ['js', 'vanilla.js', 'browser.js']}))
-                    .pipe(gconcat(bem.name('js'))));
+            return gmerge(
+                gmerge(
+                    gulp.src('node_modules/ym/modules.js'),
+                    bem.src({tech: 'js', extensions: ['js', 'vanilla.js', 'browser.js']})
+                )
+                .pipe(gconcat(bem.name('js'))),
 
-            res.push(bem.bemdecl());
+                bem.src({tech: 'styl', extensions: ['css', 'styl']})
+                    .pipe(stylus())
+                    .pipe(postcss([
+                        postcssUrl({ url: 'inline' })
+                    ]))
+                    .pipe(gconcat(bem.name('css'))),
 
-            res.push(bem.src({tech: 'styl', extensions: ['css', 'styl']})
-                .pipe(stylus())
-                .pipe(postcss([
-                    postcssUrl({ url: 'inline' })
-                ]))
-                .pipe(gconcat(bem.name('css'))));
-
-            var template = bem.src({tech: 'bemhtml', extensions: ['bemhtml.js', 'bemhtml']})
-                .pipe(through.obj(function(file, enc, cb) {
-                    if (path.basename(file.path) === 'i-bem.bemhtml') {
-                        return cb(null);
-                    }
-                    cb(null, file);
-                }))
-                //.pipe(bemxjstCompile) // for 1.x
-                .pipe(gconcat(bem.name('bemhtml.js')))
-                .pipe(bemhtml());
-
-            res.push(template);
-            // res.push(bem.html(template, bem.bemjson()));
-
-            return gmerge.apply(null, res)
-                .pipe(gulp.dest('build/desktop.bundles/index'));
+                bem.src({tech: 'bemhtml', extensions: ['bemhtml.js', 'bemhtml']})
+                    .pipe(through.obj(function(file, enc, cb) {
+                        if (path.basename(file.path) === 'i-bem.bemhtml') {
+                            return cb(null);
+                        }
+                        cb(null, file);
+                    }))
+                    //.pipe(bemxjstCompile) // for 1.x
+                    .pipe(gconcat(bem.name('bemhtml.js')))
+                    .pipe(bemhtml())
+            )
+            .pipe(gulp.dest('build/desktop.bundles/index'));
         });
 });
 
-gulp.task('build', function() {
+gulp.task('html', function() {
     var bemjson = require('./desktop.bundles/index/index.bemjson.js');
     var bemhtml = require('./build/desktop.bundles/index/index.bemhtml.js');
 
@@ -83,9 +77,6 @@ gulp.task('build', function() {
     return Promise.resolve();
 });
 
-// .pipe(through.obj(function(file, enc, cb) {
-//     console.log(file.path);
-//     cb(null, file);
-// }))
+gulp.task('build', gulp.series('jscss', 'html'));
 
-// gulp.task('default', ['build']);
+gulp.task('default', gulp.series('build'));
