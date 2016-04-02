0a4d0c2f15598c3065cb974bdb06ee16fc874962
Anton Rudeshko <rudeshko@yandex-team.ru>
2016-04-02 15:45:15 +0300

    tabs → spaces

--- gulpfile.js
+++ gulpfile.js
@@ -19,43 +19,43 @@ var conf = new Config(),
     project = bem({ bemconfig: conf.merged.levels });
 
 var bundle = project.bundle({
-	path: 'desktop.bundles/index',
-	decl: 'index.bemjson.js'
+    path: 'desktop.bundles/index',
+    decl: 'index.bemjson.js'
 });
 
 gulp.task('css', function() {
-	return bundle.src({
-		tech: 'css',
-		extensions: ['.css', '.styl']
-	})
-	.pipe(stylus())
-	.pipe(postcss([
-		autoprefixer({
-			browsers: ['ie >= 10', 'last 2 versions', 'opera 12.1', '> 2%']
-		}),
-		postcssUrl({
-			url: function(url, decl, from) {
-				return path.relative(
-					path.resolve(process.cwd(), bundle.path()),
-					path.resolve(from, url)
-				);
-			}
-		})
-	]))
-	.pipe(concat(bundle.name() + '.css'))
-	.pipe(gulp.dest(bundle.path()));
+    return bundle.src({
+        tech: 'css',
+        extensions: ['.css', '.styl']
+    })
+    .pipe(stylus())
+    .pipe(postcss([
+        autoprefixer({
+            browsers: ['ie >= 10', 'last 2 versions', 'opera 12.1', '> 2%']
+        }),
+        postcssUrl({
+            url: function(url, decl, from) {
+                return path.relative(
+                    path.resolve(process.cwd(), bundle.path()),
+                    path.resolve(from, url)
+                );
+            }
+        })
+    ]))
+    .pipe(concat(bundle.name() + '.css'))
+    .pipe(gulp.dest(bundle.path()));
 });
 
 gulp.task('js', function() {
-	return merge(
-		gulp.src(require.resolve('ym')),
-		bundle.src({
-			tech: 'js',
-			extensions: ['.js', '.vanilla.js', '.browser.js']
-		})
-	)
-	.pipe(concat(bundle.name() + '.js'))
-	.pipe(gulp.dest(bundle.path()));
+    return merge(
+        gulp.src(require.resolve('ym')),
+        bundle.src({
+            tech: 'js',
+            extensions: ['.js', '.vanilla.js', '.browser.js']
+        })
+    )
+    .pipe(concat(bundle.name() + '.js'))
+    .pipe(gulp.dest(bundle.path()));
 });
 
 gulp.task('bh', function() {
