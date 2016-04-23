d5dc4c78bf6bcfafc6a3353f6b5b87b322e0a1b0
Vladimir Grinenko <i@tadatuta.com>
2016-04-24 01:07:14 +0300

    Update dependencies

--- .enb/make.js
+++ .enb/make.js
@@ -91,7 +91,7 @@ module.exports = function(config) {
 
             // borschik
             [techs.borschik, { source: '?.js', target: '?.min.js', minify: isProd }],
-            [techs.borschik, { source: '?.css', target: '?.min.css', tech: 'cleancss', minify: isProd }]
+            [techs.borschik, { source: '?.css', target: '?.min.css', minify: isProd }]
         ]);
 
         nodeConfig.addTargets([/* '?.bemtree.js', */ '?.html', '?.min.css', '?.min.js']);
