91dbd7602474340e599c4ad21289603342bea358
Vladimir Grinenko <i@tadatuta.com>
2016-06-20 12:00:57 +0300

    enb: forceBaseTemplates for BEMHTML

--- .enb/make.js
+++ .enb/make.js
@@ -56,7 +56,10 @@ module.exports = function(config) {
             // [techs.bemtree, { sourceSuffixes: ['bemtree', 'bemtree.js'] }],
 
             // bemhtml
-            [techs.bemhtml, { sourceSuffixes: ['bemhtml', 'bemhtml.js'] }],
+            [techs.bemhtml, {
+                sourceSuffixes: ['bemhtml', 'bemhtml.js'],
+                forceBaseTemplates: true
+            }],
 
             // html
             [techs.bemjsonToHtml],
