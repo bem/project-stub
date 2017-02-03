00765fed6182817a96cbc47ec231ee3ad46d90a6
DjonyBastone <dimarikna@mail.ru>
2017-02-04 00:22:00 +0500

    Сборка Gulp - не собираются стили элемента и модификатора

---
+++ common.blocks/sitemap/sitemap.js
@@ -0,0 +1,151 @@
+/**
+ * @module popup
+ */
+
+modules.define(
+    'sitemap',
+    ['i-bem-dom'],
+    function(provide, bemDom) {
+
+        var ZINDEX_FACTOR = 1000,
+            visiblePopupsZIndexes = {},
+            undef;
+
+        /**
+         * @exports
+         * @class popup
+         * @bem
+         *
+         * @param {Number} [zIndexGroupLevel=0] z-index group level
+         *
+         * @bemmod visible Represents visible state
+         */
+        provide(bemDom.declBlock(this.name, /** @lends popup.prototype */{
+            onSetMod : {
+                'js' : {
+                    'inited' : function() {
+                        this._parentPopup = undef;
+                        this._zIndex = null;
+                        this._zIndexGroupLevel = null;
+                        this._isAttachedToScope = false;
+                    },
+
+                    '' : function() {
+                        this.delMod('visible');
+                    }
+                },
+
+                'visible' : {
+                    'true' : function() {
+                        if(!this._isAttachedToScope) {
+                            bemDom.scope.append(this.domElem);
+                            this._isAttachedToScope = true;
+                        }
+
+                        this
+                            ._captureZIndex()
+                            ._bindToParentPopup()
+                            ._domEvents().on('pointerpress pointerclick', this._setPreventHideByClick);
+
+                        this.domElem.removeAttr('aria-hidden');
+                    },
+
+                    '' : function() {
+                        this
+                            ._releaseZIndex()
+                            ._unbindFromParentPopup()
+                            ._domEvents().un('pointerpress pointerclick', this._setPreventHideByClick);
+
+                        this.domElem.attr('aria-hidden', true);
+                    }
+                }
+            },
+
+            /**
+             * Sets content
+             * @param {String|jQuery} content
+             * @returns {popup} this
+             */
+            setContent : function(content) {
+                bemDom.update(this.domElem, content);
+                return this;
+            },
+
+            _calcZIndexGroupLevel : function() {
+                var res = this.params.zIndexGroupLevel,
+                    parentPopup = this._getParentPopup();
+
+                parentPopup && (res += parentPopup._zIndexGroupLevel);
+
+                return res;
+            },
+
+            _setPreventHideByClick : function() {
+                var curPopup = this;
+                do {
+                    curPopup._preventHideByClick = true;
+                } while(curPopup = curPopup._getParentPopup());
+            },
+
+            _bindToParentPopup : function() {
+                var parentPopup = this._getParentPopup();
+                parentPopup &&
+                this._events(parentPopup).on({ modName : 'visible', modVal : '' }, this._onParentPopupHide, this);
+
+                return this;
+            },
+
+            _unbindFromParentPopup : function() {
+                this._parentPopup && this._events(this._parentPopup)
+                    .un({ modName : 'visible', modVal : '' }, this._onParentPopupHide, this);
+
+                this._parentPopup = undef;
+
+                return this;
+            },
+
+            _onParentPopupHide : function() {
+                this.delMod('visible');
+            },
+
+            _getParentPopup : function() {
+                return this._parentPopup;
+            },
+
+            _captureZIndex : function() {
+                var level = this._zIndexGroupLevel === null?
+                        this._zIndexGroupLevel = this._calcZIndexGroupLevel() :
+                        this._zIndexGroupLevel,
+                    zIndexes = visiblePopupsZIndexes[level] || (visiblePopupsZIndexes[level] = [(level + 1) * ZINDEX_FACTOR]),
+                    prevZIndex = this._zIndex;
+
+                this._zIndex = zIndexes[zIndexes.push(zIndexes[zIndexes.length - 1] + 1) - 1];
+                this._zIndex !== prevZIndex && this.domElem.css('z-index', this._zIndex);
+
+                return this;
+            },
+
+            _releaseZIndex : function() {
+                var zIndexes = visiblePopupsZIndexes[this._zIndexGroupLevel];
+                zIndexes.splice(zIndexes.indexOf(this._zIndex), 1);
+
+                return this;
+            },
+
+            _recaptureZIndex : function() {
+                this._releaseZIndex();
+                this._zIndexGroupLevel = null;
+
+                return this._captureZIndex();
+            },
+
+            _getDefaultParams : function() {
+                return {
+                    zIndexGroupLevel : 0
+                };
+            }
+        }, /** @lends popup */{
+            lazyInit : true
+        }));
+
+    });
--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,24 @@
-const path = require('path');
-
 const Builder = require('gulp-bem-bundle-builder');
 const bundler = require('gulp-bem-bundler-fs');
 const gulp = require('gulp');
+const path = require('path');
 const postcss = require('gulp-postcss');
+const mixins = require('postcss-mixins');
+const media = require('postcss-custom-media');
+// const sharps = require('sharps').postcss;
 const debug = require('gulp-debug');
+const gcmq = require('gulp-group-css-media-queries');
 const csso = require('gulp-csso');
 const filter = require('through2-filter').obj;
 const merge = require('merge2');
+const stylus = require('gulp-stylus');
 const concat = require('gulp-concat');
-const gulpif = require('gulp-if');
 const uglify = require('gulp-uglify');
 const bemhtml = require('gulp-bem-xjst').bemhtml;
 const toHtml = require('gulp-bem-xjst').toHtml;
-
-const YENV = process.env.YENV || 'development';
-const isProd = YENV === 'production';
+// const lost = require('lost');
+// const browserSync = require("browser-sync");
+// reload = browserSync.reload;
 
 const builder = Builder({
     levels: [
@@ -23,67 +26,102 @@ const builder = Builder({
         'node_modules/bem-core/desktop.blocks',
         'node_modules/bem-components/common.blocks',
         'node_modules/bem-components/desktop.blocks',
-        'node_modules/bem-components/design/common.blocks',
-        'node_modules/bem-components/design/desktop.blocks',
+        // 'node_modules/bem-components/design/common.blocks',
+        // 'node_modules/bem-components/design/desktop.blocks',
+        '.custom_libs/bove/common.blocks',
+        '.custom_libs/bove/desktop.blocks',
         'common.blocks',
         'desktop.blocks'
     ],
     techMap: {
-        bemhtml: ['bemhtml.js'],
+        bemjson: ['bemjson.js'],
+        bemhtml: [`bemhtml.js`],
         js: ['vanilla.js', 'browser.js', 'js'],
-        css: ['post.css', 'css']
+        css: ['styl', 'post.css', 'pcss', 'css']
     }
 });
+
+// var project = 'index';
+// var config = {
+//     server: {
+//         baseDir: './desktop.bundles/'+ project + '/'
+//     },
+//     files: ['./*.bundles/**/*.{html, css, js}' ],
+//     tunnel: true,
+//     host: 'localhost',
+//     port: 8080,
+//     logPrefix: "Web"
+// };
+
 
 gulp.task('build', () => {
     return bundler('*.bundles/*')
         .pipe(builder({
-            // cssdeps: bundle => bundle.src('css', {deps: true})
-            //     .pipe(concat(bundle.name + '.css.deps.js')),
-            // jsdeps: bundle => bundle.src('js', {deps: true})
-            //     .pipe(concat(bundle.name + '.js.deps.js')),
-            // bemhtmldeps: bundle => bundle.src('bemhtml', {deps: true})
-            //     .pipe(concat(bundle.name + '.bemhtml.deps.js')),
-            css: bundle =>
-                bundle.src('css')
-                    .pipe(require('gulp-one-of')())
-                    .pipe(postcss([
-                        require('postcss-import')(),
-                        require('postcss-each'),
-                        require('postcss-for'),
-                        require('postcss-simple-vars')(),
-                        require('postcss-calc')(),
-                        require('postcss-nested'),
-                        require('rebem-css'),
-                        require('postcss-url')({ url: 'inline' }),
-                        require('autoprefixer')(),
-                        require('postcss-reporter')()
-                    ]))
-                    .pipe(concat(bundle.name + '.min.css'))
-                    .pipe(gulpif(isProd, csso())),
+                // cssdeps: bundle => bundle.src('css', {deps: true})
+                //     .pipe(concat(bundle.name + '.css.deps.js')),
+                // jsdeps: bundle => bundle.src('js', {deps: true})
+                //     .pipe(concat(bundle.name + '.js.deps.js')),
+                // bemhtmldeps: bundle => bundle.src('bemhtml', {deps: true})
+                //     .pipe(concat(bundle.name + '.bemhtml.deps.js')),
+                css: bundle =>
+            bundle.src('css')
+                .pipe(stylus())
+                .pipe(require('gulp-one-of')())
+                .pipe(postcss([
+                    // require('precss')(),
+                    require('postcss-import')(),
+                    require('postcss-mixins'),
+                    require('postcss-each'),
+                    // require('postcss-simple-vars')({
+                    //     variables: config,
+                    //     silent: true
+                    // }),
+                    require('postcss-for'),
+                    require('postcss-custom-media'),
+                    // require('sharps').postcss({ columns: 12, maxWidth: '2048px', gutter: '10px',   flex: 'flex' }),
+                    require('postcss-calc')(),
+                    require('postcss-nested'),
+                    require('rebem-css'),
+                    // require('lost')(),
+                    require('postcss-url')({ url: 'inline' }),
+                    // require('autoprefixer')(),
+                    require('postcss-reporter')()
+                ]))
+                .pipe(concat(bundle.name + '.min.css'))
+                .pipe(gcmq())
+                .pipe(csso()),
             js: bundle =>
-                merge(
-                    gulp.src(require.resolve('ym')),
-                    bundle.src('js').pipe(filter(f => ~['vanilla.js', 'browser.js', 'js'].indexOf(f.tech))),
-                    bundle.src('js').pipe(filter(file => file.tech === 'bemhtml.js'))
-                        .pipe(concat('browser.bemhtml.js')).pipe(bemhtml({ elemJsInstances: true }))
-                )
-                    .pipe(concat(bundle.name + '.min.js'))
-                    .pipe(gulpif(isProd, uglify())),
-            tmpls: bundle =>
-                bundle.src('bemhtml')
-                    .pipe(concat('any.bemhtml.js'))
-                    .pipe(bemhtml({ elemJsInstances: true }))
-                    .pipe(concat(bundle.name + '.bemhtml.js')),
-            html: bundle => {
-                const bemhtmlApply = () => toHtml(bundle.target('tmpls'));
-                return gulp.src(bundle.dirname + '/*.bemjson.js')
-                    .pipe(bemhtmlApply());
-            }
-       }))
-       .on('error', console.error)
-       .pipe(debug())
-       .pipe(gulp.dest(file => path.dirname(file.path)));
+merge(
+    gulp.src(require.resolve('ym')),
+    bundle.src('js').pipe(filter(f => ~['vanilla.js', 'browser.js', 'js'].indexOf(f.tech))),
+    bundle.src('js').pipe(filter(file => file.tech === 'bemhtml.js'))
+.pipe(concat('browser.bemhtml.js')).pipe(bemhtml())
+)
+.pipe(uglify())
+    .pipe(concat(bundle.name + '.min.js')),
+    tmpls: bundle =>
+bundle.src('bemhtml')
+    .pipe(concat('any.bemhtml.js'))
+    .pipe(bemhtml())
+    .pipe(concat(bundle.name + '.bemhtml.js')),
+    html: bundle => {
+    const bemhtmlApply = () => toHtml(bundle.target('tmpls'));
+    return gulp.src(bundle.dirname + '/*.bemjson.js')
+        .pipe(bemhtmlApply());
+}
+}))
+.on('error', console.error)
+    .pipe(debug())
+    .pipe(gulp.dest(file => path.dirname(file.path)));
 });
 
-gulp.task('default', gulp.series('build'));
+// gulp.task('browser-sync', function () {
+//     browserSync(config);
+// });
+//
+// gulp.task('watch', function () {
+//     gulp.watch('./*.bundles/**/*.bemjson.js', gulp.series('build'));
+//     gulp.watch('./*.blocks/**/*.{styl,css,js}', gulp.series('build'));
+// });
+//
+// gulp.task('default', gulp.parallel(['watch', 'browser-sync']));
