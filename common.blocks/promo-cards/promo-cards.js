a12d8c3d757a93a4fccdad84da410b533c278b41
Юлия В. Музафарова <muzafarova@yamoney.ru>
2017-12-16 22:30:40 +0300

    media cards

--- common.blocks/promo-cards/promo-cards.js
+++ common.blocks/promo-cards/promo-cards.js
@@ -4,29 +4,89 @@ var offerSection = document.querySelectorAll('.promo-cards__offer-section')[0],
 	offerTab3 = document.querySelectorAll('.promo-cards__offer-tab-3')[0];
 
 offerTab1.addEventListener('click', function() {
-	offerSection.classList.add('promo-cards_slide_first')
-	offerSection.classList.remove('promo-cards_slide_second')
-	offerSection.classList.remove('promo-cards_slide_third')
+	offerTab1.classList.add('text_view_letter');
+	offerTab2.classList.remove('text_view_letter');
+	offerTab3.classList.remove('text_view_letter');
 
-	offerSection.classList.add('promo-cards__offer_type_goods')
-	offerSection.classList.remove('promo-cards__offer_type_brand')
-	offerSection.classList.remove('promo-cards__offer_type_service')
+	offerSection.classList.add('promo-cards_slide_first');
+	offerSection.classList.remove('promo-cards_slide_second');
+	offerSection.classList.remove('promo-cards_slide_third');
+
+	offerSection.classList.add('promo-cards__offer_type_goods');
+	offerSection.classList.remove('promo-cards__offer_type_brand');
+	offerSection.classList.remove('promo-cards__offer_type_service');
 });
 offerTab2.addEventListener('click', function() {
-	offerSection.classList.remove('promo-cards_slide_first')
-	offerSection.classList.add('promo-cards_slide_second')
-	offerSection.classList.remove('promo-cards_slide_third')
+	offerTab1.classList.remove('text_view_letter');
+	offerTab2.classList.add('text_view_letter');
+	offerTab3.classList.remove('text_view_letter');
 
-	offerSection.classList.remove('promo-cards__offer_type_goods')
-	offerSection.classList.add('promo-cards__offer_type_brand')
-	offerSection.classList.remove('promo-cards__offer_type_service')
+	offerSection.classList.remove('promo-cards_slide_first');
+	offerSection.classList.add('promo-cards_slide_second');
+	offerSection.classList.remove('promo-cards_slide_third');
+
+	offerSection.classList.remove('promo-cards__offer_type_goods');
+	offerSection.classList.add('promo-cards__offer_type_brand');
+	offerSection.classList.remove('promo-cards__offer_type_service');
 });
 offerTab3.addEventListener('click', function() {
-	offerSection.classList.remove('promo-cards_slide_first')
-	offerSection.classList.remove('promo-cards_slide_second')
-	offerSection.classList.add('promo-cards_slide_third')
+	offerTab1.classList.remove('text_view_letter');
+	offerTab2.classList.remove('text_view_letter');
+	offerTab3.classList.add('text_view_letter');
 
-	offerSection.classList.remove('promo-cards__offer_type_goods')
-	offerSection.classList.remove('promo-cards__offer_type_brand')
-	offerSection.classList.add('promo-cards__offer_type_service')
+	offerSection.classList.remove('promo-cards_slide_first');
+	offerSection.classList.remove('promo-cards_slide_second');
+	offerSection.classList.add('promo-cards_slide_third');
+
+	offerSection.classList.remove('promo-cards__offer_type_goods');
+	offerSection.classList.remove('promo-cards__offer_type_brand');
+	offerSection.classList.add('promo-cards__offer_type_service');
+});
+
+
+
+var mediaSection = document.querySelectorAll('.promo-cards__media-section')[0],
+	mediaCard = document.querySelectorAll('.promo-cards__media')[0],
+	mediaTab1 = document.querySelectorAll('.promo-cards__media-tab-1')[0],
+	mediaTab2 = document.querySelectorAll('.promo-cards__media-tab-2')[0],
+	mediaTab3 = document.querySelectorAll('.promo-cards__media-tab-3')[0];
+
+mediaTab1.addEventListener('click', function() {
+	mediaTab1.classList.add('text_view_letter');
+	mediaTab2.classList.remove('text_view_letter');
+	mediaTab3.classList.remove('text_view_letter');
+
+	mediaSection.classList.add('promo-cards_slide_first');
+	mediaSection.classList.remove('promo-cards_slide_second');
+	mediaSection.classList.remove('promo-cards_slide_third');
+	
+	mediaCard.classList.add('promo-cards__media_type_video');
+	mediaCard.classList.remove('promo-cards__media_type_text');
+	mediaCard.classList.remove('promo-cards__media_type_music');
+});
+mediaTab2.addEventListener('click', function() {
+	mediaTab1.classList.remove('text_view_letter');
+	mediaTab2.classList.add('text_view_letter');
+	mediaTab3.classList.remove('text_view_letter');
+
+	mediaSection.classList.remove('promo-cards_slide_first');
+	mediaSection.classList.add('promo-cards_slide_second');
+	mediaSection.classList.remove('promo-cards_slide_third');
+	
+	mediaCard.classList.remove('promo-cards__media_type_video');
+	mediaCard.classList.add('promo-cards__media_type_text');
+	mediaCard.classList.remove('promo-cards__media_type_music');
+});
+mediaTab3.addEventListener('click', function() {
+	mediaTab1.classList.remove('text_view_letter');
+	mediaTab2.classList.remove('text_view_letter');
+	mediaTab3.classList.add('text_view_letter');
+
+	mediaSection.classList.remove('promo-cards_slide_first');
+	mediaSection.classList.remove('promo-cards_slide_second');
+	mediaSection.classList.add('promo-cards_slide_third');
+	
+	mediaCard.classList.remove('promo-cards__media_type_video');
+	mediaCard.classList.remove('promo-cards__media_type_text');
+	mediaCard.classList.add('promo-cards__media_type_music');
 });
--- desktop.bundles/index/index.bemjson.js
+++ desktop.bundles/index/index.bemjson.js
@@ -131,7 +131,6 @@ module.exports = {
 			content: [
 			{
 				elem: 'theme-name',
-				// elemMods: { position: 'center' },
 				content: 'Product'
 			},
 			{
@@ -251,6 +250,363 @@ module.exports = {
 							attrs: { style: 'font-family: \'IBM Plex Mono\' !important;' },
 							mix: { block: 'promo-cards', elem: 'offer-tab-3' },
 							mods: { size: 'xl', transform: 'uppercase', view: 'primary', align: 'center' },
+							content: 'Music'
+						}
+					}]
+				}]
+			},
+			{
+				elem: 'controls',
+				content: [
+				{
+					block: 'icon',
+					mods: { 'arrow-up-2': 'xl-primary' },
+					mix: { block: 'decorator', mods: { 'indent-r': 'xxl' } }
+				},
+				{
+					block: 'icon',
+					mods: { 'arrow-down-2': 'xl-primary' }
+				}]
+			},
+			]
+		},
+		{
+			block: 'promo-cards',
+			mods: { view: 'inverse', slide: 'first' },
+			mix: { block: 'promo-cards', elem: 'media-section' },
+			content: [
+			{
+				elem: 'theme-name',
+				content: 'Video'
+			},
+			{
+				elem: 'content',
+				content: [
+				{
+					elem: 'card',
+					mix: [{ elem: 'media', elemMods: { type: 'video' } }, { block: 'decorator', mods: { 'indent-b': 'xxxxl' } }],
+					content: [
+					{
+						block: 'media',
+						mix: [{ block: 'pt-card', mods: { view: 'inverse' } }, { block: 'promo-cards', elem: 'media-video-back' }, { block: 'promo-cards', elem: 'hover' }],
+						content: [
+						{
+							elem: 'video',
+							mix: { block: 'promo-cards', elem: 'media-video' },
+							content: [
+							{
+								elem: 'play'
+							}]
+						},
+						{
+							elem: 'video-title',
+							mix: [{ block: 'pt-card', elem: 'content', elemMods: { 'space-a': 'l' } }, { block: 'promo-cards', elem: 'media-video-title' }],
+							content: [
+							{
+								block: 'text',
+								mods: { view: 'inverse', size: 'xxl' },
+								content: 'Mad Lib Theater with John Cena'
+							}]
+						},
+						{
+							elem: 'footer',
+							mix: [{ block: 'pt-card', elem: 'content', elemMods: { 'space-a': 'l' } }, { block: 'promo-cards', elem: 'media-video-footer' }],
+							content: [
+							{
+								block: 'pt-icon-plus',
+								mods: { 'vertical-align': 'center' }, 
+								content: [
+								{
+									elem: 'icon',
+									elemMods: { 'indent-right': 'xs' },
+									content: [
+									{
+										block: 'media',
+										elem: 'channel-logo'
+									}]
+								},
+								{
+									elem: 'block',
+									content: [
+									{
+										block: 'text',
+										mods: { view: 'inverse', size: 's' },
+										content: 'The Tonight Show…'
+									}]
+								}]
+							},
+							{
+								block: 'text',
+								mods: { view: 'inverse', size: 's' },
+								content: '512K views'
+							}]
+						}]
+					},
+					{
+						block: 'media',
+						mix: [{ block: 'pt-card', mods: { view: 'default' } }, { block: 'promo-cards', elem: 'media-text-back' }, { block: 'promo-cards', elem: 'hover' }],
+						content: [
+						{
+							elem: 'article-cover',
+							mix: { block: 'promo-cards', elem: 'media-cover' },
+							content: [
+							{
+								elem: 'article-author',
+								content: [
+								{
+									block: 'pt-icon-plus',
+									mods: { 'vertical-align': 'center' },
+									mix: { block: 'promo-cards', elem: 'media-author-top' },
+									content: [
+									{
+										elem: 'icon',
+										elemMods: { 'indent-right': 'xs' },
+										content: [
+										{
+											block: 'media',
+											elem: 'avatar'
+										}]
+									},
+									{
+										elem: 'block',
+										content: [
+										{
+											block: 'text',
+											mods: { view: 'inverse', size: 's' },
+											content: 'James Bridle'
+										}]
+									}]
+								}]
+							}]
+						},
+						{
+							elem: 'article-title',
+							mix: [{ block: 'pt-card', elem: 'content', elemMods: { 'space-a': 'l' } }, { block: 'promo-cards', elem: 'media-article' }],
+							content: [
+							{
+								block: 'text',
+								mods: { view: 'primary', size: 'xxl' },
+								mix: { block: 'decorator', mods: { 'indent-b': 's' } },
+								content: 'Something is wrong on the internet'
+							},
+							{
+								block: 'text',
+								mods: { view: 'primary', size: 'm' },
+								content: 'I’m James Bridle. I’m a writer and artist concerned with technology and culture. I usually write on my own blog…'
+							}]
+						},
+						{
+							elem: 'footer',
+							mix: [{ block: 'pt-card', elem: 'footer', elemMods: { 'space-a': 'l' } }, { block: 'promo-cards', elem: 'media-text-footer' }],
+							content: [
+							{
+								block: 'text',
+								mods: { view: 'secondary', size: 's' },
+								content: 'Nov 6 / 21 min read'
+							},
+							{
+								block: 'text',
+								mods: { view: 'primary', size: 's' },
+								content: '512K views'
+							}]
+						}]
+					},
+					{
+						block: 'media',
+						mix: [{ block: 'pt-card', mods: { view: 'inverse' } }, { block: 'promo-cards', elem: 'media-music-back' }, { block: 'promo-cards', elem: 'hover' }],
+						content: [
+						{
+							elem: 'music-cover',
+							mix: [{ block: 'pt-card', elem: 'content', elemMods: { 'space-a': 'l' } }, { block: 'promo-cards', elem: 'media-music-cover' }],
+							content: [
+							{
+								elem: 'music-info',
+								content: [
+								{
+									block: 'text',
+									mods: { view: 'inverse', size: 'xxl', align: 'center' },
+									mix: { block: 'decorator', mods: { 'indent-b': 'm' } },
+									content: 'Pitchfork Best New Music',
+								},
+								{
+									block: 'text',
+									mods: { view: 'inverse', size: 'm', align: 'center' },
+									content: 'Pitchfork',
+								}]
+							}]
+						},
+						{
+							block: 'playlist',
+							mix: [{ block: 'pt-list', elem: 'item', elemMods: { 'space-a': 'm' } }, { block: 'promo-cards', elem: 'media-playlist' }],
+							content: [
+							{
+								elem: 'item',
+								mix: { block: 'pt-list', elem: 'item', elemMods: { 'space-b': 'xs' } },
+								content: [
+								{
+									elem: 'track',
+									content: [
+									{
+										block: 'text',
+										mods: { view: 'inverse', size: 's' },
+										mix: { block: 'decorator', mods: { 'indent-b': 's' } },
+										content: 'Pitchfork Best New Music',
+									},
+									{
+										elem: 'description',
+										content: [
+										{
+											block: 'text',
+											mods: { view: 'inverse', size: 'xs', weight: 'light' },
+											content: 'Benjamin Clementine • At Least for Now',
+										}]
+									}]
+								},
+								{
+									block: 'text',
+									mods: { view: 'inverse', size: 's' },
+									content: '7:25'
+								}]
+							},
+							{
+								elem: 'item',
+								mix: { block: 'pt-list', elem: 'item', elemMods: { 'space-b': 'm' } },
+								content: [
+								{
+									elem: 'track',
+									content: [
+									{
+										block: 'text',
+										mods: { view: 'inverse', size: 's' },
+										mix: { block: 'decorator', mods: { 'indent-b': 'xxs' } },
+										content: 'Pitchfork Best New Music',
+									},
+									{
+										elem: 'description',
+										content: [
+										{
+											block: 'text',
+											mods: { view: 'inverse', size: 'xs', weight: 'light' },
+											content: 'Benjamin Clementine • At Least for Now',
+										}]
+									}]
+								},
+								{
+									block: 'text',
+									mods: { view: 'inverse', size: 's' },
+									content: '7:25'
+								}]
+							},
+							{
+								elem: 'item',
+								mix: { block: 'pt-list', elem: 'item', elemMods: { 'space-b': 'm' } },
+								content: [
+								{
+									elem: 'track',
+									content: [
+									{
+										block: 'text',
+										mods: { view: 'inverse', size: 's' },
+										mix: { block: 'decorator', mods: { 'indent-b': 'xxs' } },
+										content: 'Pitchfork Best New Music',
+									},
+									{
+										elem: 'description',
+										content: [
+										{
+											block: 'text',
+											mods: { view: 'inverse', size: 'xs', weight: 'light'  },
+											content: 'Benjamin Clementine • At Least for Now',
+										}]
+									}]
+								},
+								{
+									block: 'text',
+									mods: { view: 'inverse', size: 's' },
+									content: '7:25'
+								}]
+							},
+							{
+								elem: 'item',
+								mix: { block: 'pt-list', elem: 'item', elemMods: { 'space-b': 'm' } },
+								content: [
+								{
+									elem: 'track',
+									content: [
+									{
+										block: 'text',
+										mods: { view: 'inverse', size: 's' },
+										mix: { block: 'decorator', mods: { 'indent-b': 'xxs' } },
+										content: 'Pitchfork Best New Music',
+									},
+									{
+										elem: 'description',
+										content: [
+										{
+											block: 'text',
+											mods: { view: 'inverse', size: 'xs', weight: 'light' },
+											content: 'Benjamin Clementine • At Least for Now',
+										}]
+									}]
+								},
+								{
+									block: 'text',
+									mods: { view: 'inverse', size: 's' },
+									content: '7:25'
+								}]
+							}]
+						}]
+					}]
+				},
+				{
+					elem: 'description',
+					mix: { block: 'decorator', mods: { 'indent-b': 'xxxxl' } },
+					content: [
+					{
+						elem: 'header',
+						mix: { block: 'decorator', mods: { 'indent-b': 'xl' } },
+						content: 'Media Content'
+					},
+					{
+						block: 'text',
+						attrs: { style: 'font-family: \'IBM Plex Mono\' !important;' },
+						mods: { size: 'xl' },
+						content: 'Exclusive member-only offers in the run up to Black Friday, plus the best of Nike direct to your inbox. Don\'t miss out'
+					}]
+				},
+				{
+					elem: 'list',
+					content: [
+					{
+						elem: 'item',
+						mix: [{ block: 'decorator', mods: { 'indent-h': 'm' } }],
+						content: {
+							block: 'text',
+							attrs: { style: 'font-family: \'IBM Plex Mono\' !important;' },
+							mods: { size: 'xl', transform: 'uppercase', view: 'letter', align: 'center' },
+							mix: { block: 'promo-cards', elem: 'media-tab-1' },
+							content: 'Video'
+						}
+					},
+					{
+						elem: 'item',
+						mix: [{ block: 'decorator', mods: { 'indent-h': 'm' } }],
+						content: {
+							block: 'text',
+							attrs: { style: 'font-family: \'IBM Plex Mono\' !important;' },
+							mods: { size: 'xl', transform: 'uppercase', align: 'center' },
+							mix: { block: 'promo-cards', elem: 'media-tab-2' },
+							content: 'Text'
+						}
+					},
+					{
+						elem: 'item',
+						mix: [{ block: 'decorator', mods: { 'indent-h': 'm' } }],
+						content: {
+							block: 'text',
+							attrs: { style: 'font-family: \'IBM Plex Mono\' !important;' },
+							mods: { size: 'xl', transform: 'uppercase', align: 'center' },
+							mix: { block: 'promo-cards', elem: 'media-tab-3' },
 							content: 'Music'
 						}
 					}]
