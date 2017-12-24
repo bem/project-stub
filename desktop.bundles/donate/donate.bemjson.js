9e273610f955f4c4a047162326012db550776709
Кирилл Э. Федотов <kirillback@yamoney.ru>
2017-12-25 01:25:37 +0300

    merged an 'offers' with 'donates'

--- desktop.bundles/donate/donate.bemjson.js
+++ desktop.bundles/donate/donate.bemjson.js
@@ -25,20 +25,28 @@ module.exports = {
       mix: { block: 'pt-card', mods: { view: 'default' } },
       content: [
         {
-          elem: 'person-header',
-          mix: { block: 'pt-card', elem: 'header' },
+          elem: 'person-image-wrapper',
+          mix: { block: 'pt-card', elem: 'image' },
           content: [
             {
-              elem: 'person-image'
+              block: 'image',
+              url: '../../assets/i/donate-person-image.png'
             },
             {
-              elem: 'person-avatar',
+              elem: 'person-avatar-wrapper',
+              mix: { block: 'pt-card', elem: 'header' },
+              content: [
+                {
+                  elem: 'person-avatar',
+                  mix: { block: 'pt-avatar', elem: 'photo', elemMods: { size: 'm' } },
+                }
+              ]
             }
           ]
         },
         {
           elem: 'person-info',
-          mix: { block: 'pt-card', elem: 'content', elemMods: { 'space-h': 'l' } },
+          mix: { block: 'pt-card', elem: 'content', elemMods: { 'space-h': 'l', 'space-t': 'xxxl' } },
           content: [
             {
               block: 'text',
@@ -83,16 +91,15 @@ module.exports = {
       mix: { block: 'pt-card', mods: { view: 'default' } },
       content: [
         {
-          elem: 'project-header',
-          mix: { block: 'pt-card', elem: 'header' },
+          elem: 'project-image-wrapper',
+          mix: { block: 'pt-card', elem: 'image' },
           content: [
             {
-              elem: 'project-image',
-              content: [
-                {
-                  block: 'play'
-                }
-              ]
+              block: 'image',
+              url: '../../assets/i/donate-project-image.png'
+            },
+            {
+              elem: 'project-play',
             },
             {
               elem: 'project-progress-bar',
@@ -176,19 +183,133 @@ module.exports = {
               block: 'text',
               mods: { view: 'secondary', size: 'm' },
               content: 'A high-quality, hand-built tap tempo delay pedal with ' +
-              'modulated filtering, plenty of delay time, and a handful of tricks up its…'
+              'modulated filtering, plenty of delay time, and a handful …'
             }
           ]
         },
         {
           elem: 'project-action',
-          mix: { block: 'pt-card', elem: 'footer', elemMods: { 'space-a': 'm' } },
+          mix: { block: 'pt-card', elem: 'footer', elemMods: { 'space-h': 'm', 'space-v': 'l' } },
           content: [
             {
               block: 'button',
               mix: { block: 'text', mods: { view: 'inverse', size: 'm' } },
-              mods: { theme: 'islands', size: 'xl', type: 'submit', view: 'project-donate', width: 'full' },
+              mods: { theme: 'islands', size: 'xl', type: 'submit', view: 'project-donate', width: 'full', 'border-radius': 'full' },
               text: 'Back this project'
+            }
+          ]
+        }
+      ]
+    },
+
+    {
+      block: 'donate',
+      attrs: { style: 'height: 456px; margin: 100px auto;' },
+      mix: { block: 'pt-card', mods: { view: 'default' } },
+      content: [
+        {
+          elem: 'event-image-wrapper',
+          mix: { block: 'pt-card', elem: 'image' },
+          content: [
+            {
+              block: 'image',
+              url: '../../assets/i/donate-event-image.png'
+            }
+          ]
+        },
+        {
+          elem: 'event-header',
+          mix: { block: 'pt-card', elem: 'header' },
+          content: [
+            {
+              elem: 'event-avatar'
+            },
+            {
+              block: 'text',
+              mods: { view: 'primary', size: 'xxxl', weight: 'bold', style: 'italic', align: 'center', transform: 'uppercase' },
+              mix: { block: 'decorator', mods: { 'indent-b': 'xxs' } },
+              content: 'Ann’s B-day'
+            },
+            {
+              block: 'text',
+              mods: { view: 'primary', size: 's', align: 'center' },
+              content: [
+                {
+                  block: 'text',
+                  mods: { view: 'primary', size: 's', weight: 'bold', display: 'inline' },
+                  content: '25:12:08:43 '
+                },
+                {
+                  block: 'text',
+                  mods: { view: 'primary', size: 's', display: 'inline' },
+                  content: 'until the end'
+                }
+              ]
+            }
+          ]
+        },
+        {
+          elem: 'event-info',
+          mix: { block: 'pt-card', elem: 'content', elemMods: { 'space-h': 'xxl' } },
+          content: [
+            {
+              elem: 'control',
+              content: '-'
+            },
+            {
+              block: 'text',
+              mods: { view: 'primary', size: '4xl', weight: 'semibold', align: 'center' },
+              mix: { block: 'decorator', mods: { 'indent-b': 'xxs' } },
+              content: '$10'
+            },
+            {
+              elem: 'control',
+              content: '+'
+            }
+          ]
+        },
+        {
+          elem: 'event-action',
+          mix: { block: 'pt-card', elem: 'footer', elemMods: { 'space-h': 'l', 'space-v': 'xl' } },
+          content: [
+            {
+              elem: 'event-avatar-group',
+              content: [
+                {
+                  elem: 'event-mini-avatar',
+                  elemMods: { view: '1' },
+                  mix: { block: 'pt-avatar', elem: 'photo', elemMods: { size: 'xs' } },
+                },
+                {
+                  elem: 'event-mini-avatar',
+                  elemMods: { view: '2' },
+                  mix: { block: 'pt-avatar', elem: 'photo', elemMods: { size: 'xs' } },
+                },
+                {
+                  elem: 'event-mini-avatar',
+                  elemMods: { view: '3' },
+                  mix: { block: 'pt-avatar', elem: 'photo', elemMods: { size: 'xs' } },
+                },
+                {
+                  elem: 'event-mini-avatar',
+                  elemMods: { view: '4' },
+                  mix: { block: 'pt-avatar', elem: 'photo', elemMods: { size: 'xs' } },
+                }
+              ]
+            },
+            {
+              block: 'text',
+              mods: { view: 'primary', size: 'm', weight: 'semibold' },
+              content: '+6'
+            },
+            {
+              block: 'button',
+              mix: [
+                { block: 'text', mods: { view: 'inverse', weight: 'bold', transform: 'uppercase', style: 'italic' } },
+                { block: 'decorator', mods: { 'indent-l': 'xl' } }
+              ],
+              mods: { theme: 'islands', size: 'xl', type: 'submit', view: 'event-donate', 'border-radius': 'full' },
+              text: 'Make a party'
             }
           ]
         }
